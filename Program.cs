1b1d7824e4cf24fed9e988eecd8489b5894e2f84
RamiyanGangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2025-02-04 23:40:42 -0500

    Menu System

--- Program.cs
+++ Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    /// <summary>
+    /// Main menu of the program
+    /// </summary>
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Leetcode Practice Menu");
+            Console.WriteLine("=======================");
+            Console.WriteLine("1. Arrays & Hashing");
+            Console.WriteLine("0. Exit");
+            Console.Write("Select a Directory: ");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1": ArraysMenu(); break;
+                case "0": Console.WriteLine("Exiting program..."); return;
+                default: Console.WriteLine("Invalid choice, try again."); break;
+            }
+
+            Console.WriteLine("\nPress any key to return to the menu...");
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Menu for the Arrays & Hashing Section
+    /// </summary>
+    static void ArraysMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Arrays & Hashing");
+            Console.WriteLine("================");
+            Console.WriteLine("1. Contains Duplicates");
+            Console.WriteLine("0. Back to Main Menu");
+            Console.Write("Select a Program: ");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1": RunContainsDuplicate(); break;
+                case "0": return;
+                default: Console.WriteLine("Invalid choice, try again."); break;
+            }
+
+            Console.WriteLine("\nPress any key to return to the Arrays menu...");
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Display method for the Contains Duplicate problem
+    /// </summary>
+    static void RunContainsDuplicate()
+    {
+        Console.Clear();
+        Console.WriteLine("Contains Duplicate Problem");
+        Console.WriteLine("==========================");
+        Console.WriteLine("----------- WIP ----------\n");
+    }
+}
